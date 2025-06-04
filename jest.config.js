5269edb7a7d12f90fb5461f2e713fa1646dcdf6e
Barba2k2 <lorenzo.dz@hotmail.com>
2025-06-03 23:45:35 -0300

    feat: adicionar configuração do Jest para testes unitários com suporte a TypeScript e mapeamento de módulos

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/*.module.ts',
+    '!**/main.ts',
+    '!**/*.interface.ts',
+    '!**/*.dto.ts',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  // Configuração para suportar path mappings
+  moduleNameMapping: {
+    '^@domain/(.*)$': '<rootDir>/domain/$1',
+    '^@application/(.*)$': '<rootDir>/application/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/infrastructure/$1',
+    '^@presentation/(.*)$': '<rootDir>/presentation/$1',
+  },
+  // Setup para testes
+  setupFilesAfterEnv: ['<rootDir>/../test/setup.ts'],
+}; 
