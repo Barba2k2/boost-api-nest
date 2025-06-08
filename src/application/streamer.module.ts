67ae7953f2a8991270a257ff013f18e41bc0c4f3
Barba2k2 <lorenzo.dz@hotmail.com>
2025-06-08 15:57:57 -0300

    feat: add ScoreModule and integrate ScoreController with StreamerModule; implement CreateScore and GetDailyPoints use cases

--- src/application/streamer.module.ts
+++ src/application/streamer.module.ts
@@ -2,19 +2,24 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 
 // Use Cases
+import { CreateScoreUseCase } from './use-cases/streamer/create-score.use-case';
 import { CreateStreamerUseCase } from './use-cases/streamer/create-streamer.use-case';
 import { GetAllStreamersUseCase } from './use-cases/streamer/get-all-streamers.use-case';
+import { GetDailyPointsUseCase } from './use-cases/streamer/get-daily-points.use-case';
 import { GetOnlineStreamersUseCase } from './use-cases/streamer/get-online-streamers.use-case';
 import { UpdateStreamerOnlineStatusUseCase } from './use-cases/streamer/update-streamer-online-status.use-case';
 import { UpdateStreamerUseCase } from './use-cases/streamer/update-streamer.use-case';
 
 // Repository Tokens
 import { STREAMER_REPOSITORY_TOKEN } from './ports/repositories/streamer.repository.interface';
+import { SCORE_REPOSITORY_TOKEN } from './use-cases/streamer/create-score.use-case';
 
 // Repository Implementations
+import { ScoreRepository } from '@infrastructure/persistence/prisma/repositories/score.repository';
 import { StreamerRepository } from '@infrastructure/persistence/prisma/repositories/streamer.repository';
 
 // Controllers
+import { ScoreController } from '@presentation/controllers/score.controller';
 import { StreamerController } from '@presentation/controllers/streamer.controller';
 
 // External Dependencies
@@ -22,7 +27,7 @@ import { CacheRedisModule } from '../infrastructure/cache/cache.module';
 
 @Module({
   imports: [PrismaModule, CacheRedisModule],
-  controllers: [StreamerController],
+  controllers: [StreamerController, ScoreController],
   providers: [
     // Use Cases
     CreateStreamerUseCase,
@@ -30,11 +35,17 @@ import { CacheRedisModule } from '../infrastructure/cache/cache.module';
     GetOnlineStreamersUseCase,
     UpdateStreamerUseCase,
     UpdateStreamerOnlineStatusUseCase,
+    CreateScoreUseCase,
+    GetDailyPointsUseCase,
 
     // Repository Implementations
     {
       provide: STREAMER_REPOSITORY_TOKEN,
       useClass: StreamerRepository,
+    },
+    {
+      provide: SCORE_REPOSITORY_TOKEN,
+      useClass: ScoreRepository,
     },
   ],
   exports: [
@@ -43,7 +54,10 @@ import { CacheRedisModule } from '../infrastructure/cache/cache.module';
     GetOnlineStreamersUseCase,
     UpdateStreamerUseCase,
     UpdateStreamerOnlineStatusUseCase,
+    CreateScoreUseCase,
+    GetDailyPointsUseCase,
     STREAMER_REPOSITORY_TOKEN,
+    SCORE_REPOSITORY_TOKEN,
   ],
 })
 export class StreamerModule {}
