74c918c587eaff503e11699f83429edad277f75d
Barba2k2 <lorenzo.dz@hotmail.com>
2025-06-08 15:55:08 -0300

    feat: adicionar métodos para buscar usuários por email e nickname, e atualizar o último login no repositório de usuários

--- src/application/ports/repositories/user.repository.interface.ts
+++ src/application/ports/repositories/user.repository.interface.ts
@@ -6,6 +6,8 @@ export interface CreateUserData {
   nickname: string;
   password: string;
   role: string;
+  email: string;
+  fullName: string;
 }
 
 export interface UpdateTokensData {
@@ -18,6 +20,12 @@ export interface IUserRepository {
   create(userData: CreateUserData): Promise<User>;
   findById(id: number): Promise<User | null>;
   findByNickname(nickname: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  findByEmailOrNickname(emailOrNickname: string): Promise<User | null>;
   updateTokens(id: number, tokens: UpdateTokensData): Promise<User>;
+  updateLastLogin(id: number): Promise<User>;
   existsByNickname(nickname: string): Promise<boolean>;
+  existsByEmail(email: string): Promise<boolean>;
+  findUsersWithLogin(limit: number, offset: number): Promise<User[]>;
+  countUsersWithLogin(): Promise<number>;
 }
