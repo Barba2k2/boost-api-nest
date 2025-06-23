227658c90e5b0953719e7b82bfe1d37d3475edb3
Barba2k2 <lorenzo.dz@hotmail.com>
2025-06-23 10:51:16 -0300

    feat(user): update update-last-login.use-case.ts

--- src/application/use-cases/user/update-last-login.use-case.ts
+++ src/application/use-cases/user/update-last-login.use-case.ts
@@ -1,5 +1,5 @@
 import { User } from '@domain/entities/user.entity';
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import {
   IUserRepository,
   USER_REPOSITORY_TOKEN,
@@ -7,12 +7,29 @@ import {
 
 @Injectable()
 export class UpdateLastLoginUseCase {
+  private readonly logger = new Logger(UpdateLastLoginUseCase.name);
+
   constructor(
     @Inject(USER_REPOSITORY_TOKEN)
     private readonly userRepository: IUserRepository,
   ) {}
 
   async execute(userId: number): Promise<User> {
-    return await this.userRepository.updateLastLogin(userId);
+    this.logger.log(
+      `[UPDATE_LAST_LOGIN] Atualizando último login para usuário ID: ${userId}`,
+    );
+
+    try {
+      const user = await this.userRepository.updateLastLogin(userId);
+      this.logger.log(
+        `[UPDATE_LAST_LOGIN_SUCCESS] Último login atualizado com sucesso - ID: ${user.id}, Nick: ${user.nickname}, Novo lastLogin: ${user.lastLogin}`,
+      );
+      return user;
+    } catch (error) {
+      this.logger.error(
+        `[UPDATE_LAST_LOGIN_ERROR] Erro ao atualizar último login - UserID: ${userId}, Erro: ${error.message}`,
+      );
+      throw error;
+    }
   }
 }
